37ecc0e7539eb86b4e9c80e8b5e99815eddcff91
May Jiang <mayjiang@princeton.edu>
2017-06-19 16:16:17 -0400

    Input screen

--- Input.js
+++ Input.js
@@ -1,28 +1,126 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
+import { View, Text, StyleSheet, TouchableHighlight,
+  TextInput, KeyboardAvoidingView } from 'react-native';
 
 export default class InputScreen extends Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View style={{flex: 1}}>
+      /*<View style={{flex: 1}}>
         <View style={styles.header} >
          <Text style={styles.title}>Add Event</Text>
+        </View>*/
+      <KeyboardAvoidingView behavior="padding">
+        <View style={styles.taskbar}>
+          <Text style={styles.headingText}>ENTER INFO</Text>
         </View>
-        <View style={styles.body} >
-         <Text style={styles.item}>Add Event!</Text>
+        <View style={styles.container}>
+          <View >
+            <Text style={styles.inputText}>Title</Text>
+          </View>
+          <TextInput
+            style={styles.normInput}
+          />
         </View>
+
+        <View style={styles.container}>
+          <View >
+            <Text style={styles.inputText}>Who</Text>
+          </View>
+          <TextInput
+            style={styles.normInput}
+          />
+        </View>
+
+        <View style={styles.container}>
+          <View >
+            <Text style={styles.inputText}>Place</Text>
+          </View>
+          <TextInput
+            style={styles.normInput}
+          />
+        </View>
+
+        <View style={styles.container}>
+          <View >
+            <Text style={styles.inputText}>Description</Text>
+          </View>
+          <TextInput
+            multiline
+            style={styles.descriptionInput}
+          />
+        </View>
+
+        <TouchableHighlight style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>SUBMIT</Text>
+        </TouchableHighlight>
+
         <View style={styles.footer}>
-         <TouchableHighlight style={styles.button}
+         <TouchableHighlight style={styles.buttonContainer}
          onPress={() => navigate('Home')} underlayColor='#ffd199'>
           <Text style={styles.buttonText}>Back</Text>
          </TouchableHighlight>
         </View>
-      </View>
+
+      </KeyboardAvoidingView>
+
     );
   }
 }
 
+const styles = StyleSheet.create({
+  taskbar:{
+    marginTop:20,
+    padding:30,
+    flex:1,
+    backgroundColor:'#f9a56a',
+    justifyContent:'center',
+    alignItems: 'center'
+
+  },
+  headingText:{
+    color:'white',
+    fontSize:30,
+    fontWeight:'500',
+    fontFamily:'AvenirNext-Regular'
+  },
+  container:{
+    marginTop:20,
+    marginBottom:0,
+    marginLeft:15,
+    marginRight:15,
+    padding:10,
+    backgroundColor:'#f9a56a'
+  },
+  inputText:{
+    fontSize:15,
+    fontFamily:'AvenirNext-Regular',
+    color:'white'
+  },
+  normInput:{
+    height:40,
+    backgroundColor:'white',
+    padding:5
+  },
+  descriptionInput:{
+    height:100,
+    backgroundColor:'white',
+    padding:5,
+
+  },
+  buttonContainer:{
+    backgroundColor:'#727373',
+    padding:10,
+    margin: 20
+  },
+  buttonText:{
+    textAlign:'center',
+    color:'white',
+    fontWeight:'500'
+  }
+});
+
+/*
 const styles = StyleSheet.create({
   header: {
     paddingTop: 20,
@@ -58,4 +156,4 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 20,
   }
-})
+})*/
