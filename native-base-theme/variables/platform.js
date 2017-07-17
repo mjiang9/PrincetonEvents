977e4d93472d734d106669a70ddb6b333a532ac0
Viraj Deokar <vrjdeokar@gmail.com>
2017-07-17 12:18:51 -0400

    fixed conflict in platform theme

--- native-base-theme/variables/platform.js
+++ native-base-theme/variables/platform.js
@@ -109,7 +109,6 @@ export default {
 
   // Color
   brandPrimary: platform === "ios" ? "#f9a56a" : "#f9a56a",
-  brandPrimary: platform === "ios" ? "#007aff" : "#3F51B5",
   brandInfo: "#62B1F6",
   brandSuccess: "#5cb85c",
   brandDanger: "#d9534f",
