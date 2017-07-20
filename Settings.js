bc33bbeb7fe569b9adcef3ffc7eaa8e117be04dd
Viraj Deokar <vrjdeokar@gmail.com>
2017-07-20 12:40:12 -0400

    added log out in settings

--- Settings.js
+++ Settings.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import {firebaseApp} from './App';
+import { StyleProvider, Container, Header, Title, Content, Button,
+  Left, Right, Body, Icon, Text, List, ListItem } from 'native-base';
+import getTheme from './native-base-theme/components';
+import material from './native-base-theme/variables/material';
+
 export default class AnatomyExample extends Component {
+
+  logout = () => {
+    firebaseApp.auth().signOut().then(() => {
+      this.props.navigation.navigate('LogOut');
+      console.log('logged out');
+    }).catch((error) => {
+      alert(error.code);
+    })
+  };
+
   render() {
     return (
+    <StyleProvider style={getTheme(material)}>
       <Container>
         <Header>
           <Left>
-            <Button transparen onPress={() => this.props.goBack()}>
+            <Button transparent onPress={() => this.props.goBack()}>
               <Icon name='arrow-back' />
             </Button>
           </Left>
           <Body>
-            <Title>Header</Title>
+            <Title>Settings</Title>
           </Body>
           <Right />
         </Header>
-        <Content>
-          <Text>
-            This is Content Section
-          </Text>
+        <Content style={{backgroundColor: 'white'}}>
+          <List>
+            <ListItem icon onPress={() => this.logout()}>
+              <Left>
+                <Icon name="power" />
+              </Left>
+              <Body>
+                <Text>Log Out</Text>
+              </Body>
+            </ListItem>
+          </List>
         </Content>
       </Container>
+    </StyleProvider>
     );
   }
 }
